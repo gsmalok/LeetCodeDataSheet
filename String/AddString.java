b379569f56f2b29e65483ffbb6edc05fb5201aa2
gsmalok <gsmalok@gmail.com>
2022-12-25 21:58:10 +0530

    BigIneger add two String  as number

---
+++ String/AddString.java
@@ -0,0 +1,19 @@
+package String;
+import java.math.BigInteger;
+public class AddString {
+
+             public static String AddStringBigInteger(String num1, String num2) {
+                BigInteger  input1 = new BigInteger(num1);
+                BigInteger input2 = new BigInteger(num2);
+                 
+                BigInteger Sum ; 
+                Sum = input1.add(input2);
+                return Sum.toString();
+             }
+    public static void main(String[] args) {
+        String str1 = "123";
+        String str2 = "1";
+        System.out.println(AddStringBigInteger(str1, str2));
+
+    }
+}
