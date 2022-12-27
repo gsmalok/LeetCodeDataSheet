f976cb110bdd314fb4d46f82c467ff3ab3919a03
gsmalok <gsmalok@gmail.com>
2022-12-27 21:25:27 +0530

    no

--- String/WhyImmutable.java
+++ String/WhyImmutable.java
@@ -3,9 +3,9 @@ package String;
 public class WhyImmutable {
     public static void main(String[] args) {
         String str = "Sachin";
-       str = str.concat(" Tendulakar");
+       str = str.concat(" Tendulakar"); // it refer to sachin Tendulakr
 
-       //   str.concat(" Tendulakar");  // Immutable Concept
+       //   str.concat(" Tendulakar");  // Immutable Concept // It reffer to scahin 
 
         System.out.println(str);
     }
