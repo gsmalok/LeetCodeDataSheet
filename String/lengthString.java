cfc562ebcda67d46c0e459de775caf707460ee3c
gsmalok <gsmalok@gmail.com>
2022-12-30 16:29:49 +0530

    convent first letter of word as capital

---
+++ String/lengthString.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.*;
+
+public class lengthString {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner (System.in);
+
+        String  A = scn.next();
+        String B = scn.next();
+
+        String C = A + B;
+        System.out.println(C.length());
+
+        System.out.println(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
+
+
+    }
+}
