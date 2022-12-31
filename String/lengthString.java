e14e34041eb6877f2d7345e6b1fbc2791f9f7099
gsmalok <gsmalok@gmail.com>
2022-12-31 20:17:18 +0530

    ok

--- String/lengthString.java
+++ String/lengthString.java
@@ -13,7 +13,7 @@ public class lengthString {
         System.out.println(C.length());
 
         System.out.println(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
-
-
+        
+           // 
     }
 }
