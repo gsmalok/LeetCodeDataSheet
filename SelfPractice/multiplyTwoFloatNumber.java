478ace6c6bdedd36c2d9e80c9ea2f64df99581a2
gsmalok <gsmalok@gmail.com>
2023-01-06 09:56:43 +0530

    multiplyTwoFloatNumber

---
+++ SelfPractice/multiplyTwoFloatNumber.java
@@ -0,0 +1,13 @@
+package SelfPractice;
+
+public class multiplyTwoFloatNumber {
+    public static void main(String[] args) {
+
+        float p1 = 98.08F;
+        float p2 = 87.64F;
+        float result = p1 * p2;
+        System.out.println(result);
+
+    }
+
+}
