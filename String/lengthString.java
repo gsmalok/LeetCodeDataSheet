aae47a5326d317715127fb95d035211c788d5001
gsmalok <gsmalok@gmail.com>
2023-01-07 23:06:45 +0530

    ok

--- String/lengthString.java
+++ String/lengthString.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 public class lengthString {
     public static void main(String[] args) {
-        Scanner scn = new Scanner (System.in);
+        try (Scanner scn = new Scanner (System.in)) {
+            String  A = scn.next();
+            String B = scn.next();
 
-        String  A = scn.next();
-        String B = scn.next();
+            String C = A + B;
+            System.out.println(C.length());
 
-        String C = A + B;
-        System.out.println(C.length());
-
-        System.out.println(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
+            System.out.println(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
+        }
     }
 }
 
