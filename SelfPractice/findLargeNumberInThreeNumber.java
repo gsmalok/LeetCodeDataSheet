6a5a1e9c9328bacd5a11087ad796c7397f7c709a
gsmalok <gsmalok@gmail.com>
2023-01-07 23:05:04 +0530

    inn three number the large number is

---
+++ SelfPractice/findLargeNumberInThreeNumber.java
@@ -0,0 +1,13 @@
+package SelfPractice;
+
+public class findLargeNumberInThreeNumber {
+    public static void main(String[] args) {
+            
+             int x = 10;
+             int y = 43;
+             int z = 34;
+
+             int largeNumber = x>((y>z)?y:z)?x:((y>z)?y:z);
+             System.out.println(largeNumber);
+    }
+}
