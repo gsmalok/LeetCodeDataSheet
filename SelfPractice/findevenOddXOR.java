dcb97a5bce9778a9ac3b6fe132c07ea0bfca0dba
gsmalok <gsmalok@gmail.com>
2023-01-07 07:39:30 +0530

    ok

--- SelfPractice/findevenOddXOR.java
+++ SelfPractice/findevenOddXOR.java
@@ -2,7 +2,6 @@ package SelfPractice;
 
 public class findevenOddXOR {
     public static void main(String[] args) {
-        
         int number = 99092;
         if((number ^ 1) == (number + 1)){
             System.out.println("Even");
