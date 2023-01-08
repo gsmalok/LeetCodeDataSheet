64050abbc753064ba65c973de47feef8c8513a33
gsmalok <gsmalok@gmail.com>
2023-01-08 18:10:31 +0530

    find strong number

---
+++ numberisSrtong.java
@@ -0,0 +1,14 @@
+class numberisSrtong{
+    public static void main(String[] args) {
+        int number = 10;
+
+        int count = 0;
+
+        for(int i=1; i<=number; i++){
+            if(i%5==0 || i%7==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
