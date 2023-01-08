f25c429ba3236400e9152f2d5a14934a5addc02f
gsmalok <gsmalok@gmail.com>
2023-01-09 03:33:02 +0530

    Armanstrong Number

---
+++ SelfPractice/Armstrong_Number.java
@@ -0,0 +1,30 @@
+package SelfPractice;
+import java.util.*;
+public class Armstrong_Number {
+    public static void main(String[] args) {
+        
+        // an integer such that the sum of the cubes of its digits is equal to the number itself. 
+        // example is 371 
+        
+        int number = 54749;
+        int temp = number;
+        int temp1 = number;
+        int count = 0; 
+        double arm = 0;
+        while(temp1!=0){
+            temp1 = temp1/10;
+            count++;
+        }
+        while(temp != 0){
+            double rem = temp%10;
+             arm = arm + Math.pow(rem,count );
+             temp=temp/10;
+        }
+
+        if(number==arm){
+            System.out.println("It is Armngstrong Numner");
+        }else{
+            System.out.println("Not Armangstrong");
+        }
+    }
+}
