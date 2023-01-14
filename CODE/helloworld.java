3af82c9e8f411cfd087009a29e79d760d5fbaa80
gsmalok <gsmalok@gmail.com>
2023-01-14 19:47:36 +0530

    ok

--- CODE/helloworld.java
+++ CODE/helloworld.java
@@ -1,15 +1,16 @@
 package CODE;
+
 // import javax.naming.ldap.SortControl;
 public class helloworld {
-    public class A{
-        static int add(int i, int j){
-          return i+j;
+    public class A {
+        static int add(int i, int j) {
+            return i + j;
         }
     }
-    public class B extends A{
+    public class B extends A {
         public static void main(String[] args) {
-                short s = 9;
-            System.out.println(add(s,6));  
+            short s = 9;
+            System.out.println(add(s, 6));
         }
     }
 }
