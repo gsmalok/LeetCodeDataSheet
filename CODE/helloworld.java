60c0ba2aa63b9971a0734710976e8e5b751f04d4
gsmalok <gsmalok@gmail.com>
2023-01-14 19:43:21 +0530

    ok

--- CODE/helloworld.java
+++ CODE/helloworld.java
@@ -10,6 +10,7 @@ public class helloworld {
         public static void main(String[] args) {
                 short s = 9;
                 System.out.println(add(s,6));
+                
         }
     }
 }
